888c5ea0b8c27d606c807d57651551f0aa1492a3
Enrico Farina <enrico.farina.16@gmail.com>
2024-11-15 12:45:36 +0100

    Add project Hello World

---
+++ Geometria-Hello-World/src/main/java/it/test/geometriahelloworld/GeometriaHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package it.test.geometriahelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeometriaHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeometriaHelloWorldApplication.class, args);
+	}
+
+}
